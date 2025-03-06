f93b4a32e2022beccf5fab44d46687b767d38178
Kimuu76 <officialkimutai@gmail.com>
2025-03-06 10:38:50 +0300

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,54 @@
+/** @format */
+
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { connectDB } = require("./config/db");
+const path = require("path");
+const setupRoutes = require("./routes/setup");
+const app = express();
+const loginRoutes = require("./routes/login");
+const dashboardRoutes = require("./routes/login");
+const purchaseReturnsRoutes = require("./routes/purchaseReturns");
+const productsRoutes = require("./routes/products");
+const purchasesRoutes = require("./routes/purchases");
+const reportsRoutes = require("./routes/reports");
+const salesRoutes = require("./routes/sales");
+const salesReturnsRoutes = require("./routes/salesReturns");
+const stockPricesRoutes = require("./routes/stockPrices");
+const suppliersRoutes = require("./routes/suppliers");
+const companyRoutes = require("./routes/company");
+const createTables = require("./models/initTables");
+
+const PORT = process.env.PORT || 5000;
+
+app.use(
+	cors({
+		origin: "https://pos.onrender.com",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true, // Allow cookies
+		allowedHeaders: ["Content-Type", "Authorization"], // Make sure Authorization is allowed
+	})
+);
+
+app.use(bodyParser.json());
+
+//  Routes
+app.use("/setup", setupRoutes);
+app.use("/login", loginRoutes);
+app.use("/dashboard", dashboardRoutes);
+app.use("/purchase-returns", purchaseReturnsRoutes);
+app.use("/products", productsRoutes);
+app.use("/sales", salesRoutes);
+app.use("/sales-returns", salesReturnsRoutes);
+app.use("/stockPrices", stockPricesRoutes);
+app.use("/supplier", suppliersRoutes);
+app.use("/reports", reportsRoutes);
+app.use("/purchases", purchasesRoutes);
+app.use("/company", companyRoutes);
+
+app.listen(PORT, async () => {
+	await createTables();
+	await connectDB();
+	console.log(`Server running on port ${PORT}`);
+});
